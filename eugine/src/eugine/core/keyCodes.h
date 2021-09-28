0b0fa2ab3d10a20b7f213927c4531a13613e8ad8
aiden <alambert29@ladueschools.net>
2021-09-28 06:44:13 -0500

    a commit

---
+++ eugine/src/eugine/core/keyCodes.h
@@ -0,0 +1,8 @@
+//
+// Created by aiden on 9/25/21.
+//
+
+#ifndef EUGINE_KEYCODES_H
+#define EUGINE_KEYCODES_H
+
+#endif //EUGINE_KEYCODES_H
---
+++ eugine/src/eugine/core/mouseCodes.h
@@ -0,0 +1,8 @@
+//
+// Created by aiden on 9/25/21.
+//
+
+#ifndef EUGINE_MOUSECODES_H
+#define EUGINE_MOUSECODES_H
+
+#endif //EUGINE_MOUSECODES_H
