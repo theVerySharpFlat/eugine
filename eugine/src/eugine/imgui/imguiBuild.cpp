6fa2051b99b640bb0bba5a3be35782df21ae1f21
aiden <alambert29@ladueschools.net>
2021-10-15 20:16:36 -0500

    improved imgui

--- eugine/src/eugine/imgui/imguiBuild.cpp
+++ eugine/src/eugine/imgui/imguiBuild.cpp
@@ -2,7 +2,7 @@
 // Created by aiden on 10/8/21.
 //
 
-#include "backends/imgui_impl_opengl3_loader.h"
-#include "backends/imgui_impl_glfw.cpp"
-#include "backends/imgui_impl_opengl3.cpp"
+#include "imgui/backends/imgui_impl_opengl3_loader.h"
+#include "imgui/backends/imgui_impl_glfw.cpp"
+#include "imgui/backends/imgui_impl_opengl3.cpp"
 
--- eugine/src/eugine/imgui/imguiLayer.cpp
+++ eugine/src/eugine/imgui/imguiLayer.cpp
@@ -5,11 +5,10 @@
 #include <eugine/core/application.h>
 #include "imguiLayer.h"
 
-#include "backends/imgui_impl_glfw.h"
-#include "backends/imgui_impl_opengl3.h"
+#include "imgui/backends/imgui_impl_glfw.h"
+#include "imgui/backends/imgui_impl_opengl3.h"
 
 #include "GLFW/glfw3.h"
-#include <glad/glad.h>
 
 enum GlfwClientApi
         {
